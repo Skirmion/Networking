cbbd311f0c4f10125fdc4c1c1e4a997f0185c219
Skirmion <71891194+Skirmion@users.noreply.github.com>
2021-05-21 18:05:29 +0300

    Update Phys.cpp

--- Phys.cpp
+++ Phys.cpp
@@ -5,9 +5,23 @@
 #include "GameObj.hpp"
 #include "Phys.hpp"
 
-bool isCollide(Collider* one)
+bool isCollide(sf::ConvexShape collisionModel1, sf::ConvexShape collisionModel2)
 {
-
+    for (int i = 0; i < collisionModel1.getPointCount(); i++)
+    {
+        for (int j = 0; j < collisionModel2.getPointCount(); j++)
+        {
+            if ((collisionModel1.getPoint(i).x <= collisionModel2.getPoint(j).x <= collisionModel1.getPoint(i + 1).x) or (collisionModel1.getPoint(i).x <= collisionModel2.getPoint(j + 1).x <= collisionModel1.getPoint(i + 1).x))
+            {
+                if ((collisionModel1.getPoint(i).y <= collisionModel2.getPoint(j).y <= collisionModel1.getPoint(i + 1).y) or (collisionModel1.getPoint(i).y <= collisionModel2.getPoint(j + 1).y <= collisionModel1.getPoint(i + 1).y))
+                {
+                    return true;
+                }
+            }
+                
+        }
+    }
+    return false;
 }
 
 void resolutionCollision(Collider* one)
