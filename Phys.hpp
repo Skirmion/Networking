f618dbded7c25c354387987a56934711b71c23d7
Skirmion <71891194+Skirmion@users.noreply.github.com>
2021-05-21 17:16:01 +0300

    Update Phys.hpp

--- Phys.hpp
+++ Phys.hpp
@@ -6,6 +6,14 @@
 #include "Component.hpp"
 #include <SFML/Graphics.hpp>
 
+class Physics : public Component
+{
+	float m = 0;
+	sf::Vector2f position = sf::Vector2f(0, 0);
+	sf::Vector2f velocity = { 0,0 };
+	sf::Vector2f boost = {0, 0};
+};
+
 class Collider :public Component {
 private:
 	std::list<sf::ConvexShape> collisionModel;
